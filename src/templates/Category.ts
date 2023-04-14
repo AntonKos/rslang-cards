1412aea3df566c6c06747cf54f1bcf95e5ba6713
Anton <antonkosicyn@rambler.ru>
2023-04-14 19:26:53 +0300

    refactor: delete link from category

--- src/templates/Category.ts
+++ src/templates/Category.ts
@@ -1,11 +1,9 @@
 const Category = (data:any) => `
 <div class="card__main ${data.isTrain ? 'train' : 'game'}">
-<a href="#${data.hash}">
     <div class="card__main-item">
 
         <a class="card__main-name" href="#${data.hash}">${data.name}</a>
     </div>
-</a>
 </div>
 `;
 
