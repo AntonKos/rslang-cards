ecfc100b05f5f217d844b5d9483c7006c92c346f
Anton <antonkosicyn@rambler.ru>
2023-04-18 13:48:18 +0300

    feat: add audio tag in repeat button function

--- src/templates/RepeatButton.ts
+++ src/templates/RepeatButton.ts
@@ -1,7 +1,12 @@
 const RepeatButton = () => `
 <button class="repeat" type="button">repeat</button>
 
+<audio
+  controls
+  src="/media/cc0-audio/t-rex-roar.mp3">
+</audio>
+
 <button class="quote-button" type="button">Play Quote</button>
 `;
 
-export default RepeatButton; // убрал svg
+export default RepeatButton;
